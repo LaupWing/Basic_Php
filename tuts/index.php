24435facf273ba6f7ab8b5c0011284cf1aab8691
LaupWing <loc_strict@hotmail.com>
2020-03-22 16:08:02 +0100

    added own styling

--- tuts/index.php
+++ tuts/index.php
@@ -1,11 +1,9 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Test
+
+    <?php includes('partials/header.php')?>
+    
 </body>
-</html>
--- tuts/partials/header.php
+++ tuts/partials/header.php
@@ -3,6 +3,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tutorial</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style type="text/css">
+        .brand{
+            background: #cbb09c !important;
+        }
+        .brand-text{
+            color: #cbb09c !important;
+        }
+    </style>
 </head>
 <body class="grey lighten-4">
     <nav class="white z-depth-0">
