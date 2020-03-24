022f89ed3f193fe053a11768c5adb73fdd5502ca
LaupWing <loc_strict@hotmail.com>
2020-03-24 18:00:22 +0100

    form validation

--- tuts/add.php
+++ tuts/add.php
@@ -1,8 +1,35 @@
 <?php
     if(isset($_POST['submit'])){
-        echo htmlspecialchars($_POST['email']);
-        echo htmlspecialchars($_POST['title']);
-        echo htmlspecialchars($_POST['ingredients']);
+        // echo htmlspecialchars($_POST['email']);
+        // echo htmlspecialchars($_POST['title']);
+        // echo htmlspecialchars($_POST['ingredients']);
+        if(empty($_POST['email'])){
+            echo 'An email is required <br/>'
+        }else{
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                echo 'This is not a valid email';
+            }
+            // echo htmlspecialchars($_POST['email']);
+        }
+        if(empty($_POST['title'])){
+            echo 'An title is required <br/>'
+        }else{
+            $title = $_POST['title'];
+            if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+                echo 'Title must be letters and spaces only';
+            }
+            // echo htmlspecialchars($_POST['title']);
+        }
+        if(empty($_POST['ingredients'])){
+            echo 'An ingredients is required <br/>'
+        }else{
+            $ingredients = $_POST['ingredients'];
+            if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+                echo 'Ingredients must be comma seperated';
+            }
+            // echo htmlspecialchars($_POST['ingredients']);
+        }
     }
 ?>
 <!DOCTYPE html>
