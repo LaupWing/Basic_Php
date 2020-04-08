e9a53443acb736d1be900ae3ee0036fb93cc7e42
LaupWing <loc_strict@hotmail.com>
2020-04-08 19:10:32 +0200

    delted

--- tuts/details.php
+++ tuts/details.php
@@ -1,5 +1,16 @@
 <?php
     include('config/db_connect.php');
+    if(isset($_POST['delete'])){
+        print('Deleting');
+        $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+        $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+        if(mysqli_query($conn, $sql)){
+            header('Location: index.php');
+        }else {
+            echo 'quyer error ' . mysqli_error($conn);
+        }
+    }
     if(isset($_GET['id'])){
         $id = mysqli_real_escape_string($conn, $_GET['id']);
 
@@ -11,7 +22,6 @@
 
         mysqli_free_result($result);
         mysqli_close($conn);
-        print_r($pizza);
     }
 ?>
 <!DOCTYPE html>
@@ -25,6 +35,10 @@
             <p> <?php echo date($pizza['created_at']);?> </p>
             <h5>Ingredients:</h5>
             <p><?php htmlspecialchars($pizza['ingredients']); ?></p>
+            <form action="details.php" method="POST">
+                <input name="id_to_delete" type="hidden" value="<?php echo $pizza['id'];?>">
+                <button name="delete" type="submit" class="btn brand z-depth-0">Delete</button>
+            </form>
         <?php else:?>
             <h5>NO PIZZA FOUND</h5>
         <?php endif;?>
